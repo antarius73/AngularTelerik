7d22325db6c9e8d8ead3c48f5c46e53ab107efff
antarius73 <gjonik@gmail.com>
2016-04-21 08:58:58 +0200

    Creation git

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kendoTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kendoTestApp
+ */
+angular.module('kendoTestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kendoTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kendoTestApp
+ */
+angular.module('kendoTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
