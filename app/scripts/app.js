969d41186ba11c51c150d68ce35a17935f1540ef
antarius73 <gjonik@gmail.com>
2016-04-25 17:45:12 +0200

    debut de grid kendo avec deux champs affichées

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,7 +19,10 @@ angular
     'kendo.directives'
   ])
     .constant('WCF_URL_BASE', 'https://svr-grind.tesfri.intra:8081')
-
+    .config(['$resourceProvider', function ($resourceProvider) {
+        // Don't strip trailing slashes from calculated URLs
+        $resourceProvider.defaults.stripTrailingSlashes = false;
+    }])
     .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
@@ -45,4 +48,18 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+    .run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+        $rootScope.$on('$locationChangeStart', function () {
+            // redirect to login page if not logged in
+            if ($location.path() !== '/login' && (!$rootScope.globals.currentUser)) {
+                $location.path('/login');
+            }
+        });
+    }]);
